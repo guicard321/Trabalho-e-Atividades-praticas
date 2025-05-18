27a86bd88f53ac35a960ab4453774a6e56206c41
guicard321 <guilherme_pc3@hotmail.com>
2025-05-18 18:28:24 -0300

    Add files via upload

---
+++ IDE VS Code/estruturas_condicao1 (atividade 1).py
@@ -0,0 +1,6 @@
+temperatura = 31
+
+if temperatura < 30:
+    print("A temperatura hoje está amena")
+else:
+    print("hoje esta fazendo calor")
---
+++ IDE VS Code/estruturas_repeticao2 (atividade 4).py
@@ -0,0 +1,8 @@
+
+texto = 'Olá, laço for.'
+    
+for item in texto:
+        print('Caractere: ' + item)
+    
+for numero in range(1, 10):
+        print('Número do intervalo: ' + str(numero))
